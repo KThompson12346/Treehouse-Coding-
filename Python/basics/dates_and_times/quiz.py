8507ae97e2a96f3d3a0f47d50b21913a7c26709d
KThompson <kt3223@gmail.com>
2019-03-11 18:36:40 +0000

    quiz game complete
    completed videos on creating a quiz game using classes

--- Python/basics/dates_and_times/quiz.py
+++ Python/basics/dates_and_times/quiz.py
@@ -10,24 +10,57 @@ class Quiz:
 
     def __init__(self):
         # generate 10 random questions with numbers from 1 to 10
+        question_types = (Add, Multiply)
+        for _ in range(10):
+            num1 = random.randint(1, 10) # a random integer from 1 to 10
+            num2 = random.randint(1, 10)
+            question = random.choice(question_types)(num1, num2) # random.choice() chooses at random a question type from the classes Add and Multiply and takes num1 and num2 as the arguments
+            self.questions.append(question)
         # add these questions into self.questions
 
     def take_quiz(self):
         # log the start time
+        self.start_time = datetime.datetime.now()
         # ask all the questions
-        # log if they got the questions right
-        # log the end time
+        for question in self.questions:
+            # log if they got the questions right
+            self.answers.append(self.ask(question))
+        else:
+            # log the end time
+            self.end_time = datetime.datetime.now()
         # show a summary
+        return self.summary()
+
 
     def ask(self, question):
+        correct = False
         # log start time
+        question_start = datetime.datetime.now()
         # capture the answer
+        answer = input(question.text + ' = ')
         # check the answer
+        if answer == str(question.answer):
+            correct = True
         # log the end time
+        question_end = datetime.datetime.now()
         # if the answer's right, send back True
         # otherwise, send back False
         # send back the elapsed time, too
+        return correct, question_end - question_start
+
+    def total_correct(self):
+        # return the total # of correct answers
+        total = 0
+        for answer in self.answers:
+            if answer[0]: # is saying if an answer is True i.e. 'right'
+                total += 1 # the total is increment by 1 to indicate a right answer
+        return total # returns the total number of correct answers
 
     def summary(self):
         # print how many you got right and the total number of question.
+        print("You got {} out of {} right.".format(self.total_correct(), len(self.questions)))
         # print the total time for the quiz: 30 seconds
+        print("It took you {} seconds total.".format((self.end_time - self.start_time).seconds))
+
+
+Quiz().take_quiz()
