650597e445ac865926208709bfab1a95234fc81c
KThompson <kt3223@gmail.com>
2019-04-29 18:58:23 +0100

    Update frame_widget.py

--- Python/intermediate/GUI/frame_widgets/frame_widget.py
+++ Python/intermediate/GUI/frame_widgets/frame_widget.py
@@ -28,7 +28,7 @@ entry_county = Entry(frame_address)
 submit_name = Button(frame_name, text='     Submit     ')
 submit_address = Button(frame_address, text='     Submit     ')
 
-# lines 30-36 positioning of the widgets within the frame_name frame
+# lines 32-38 positioning of the widgets within the frame_name frame
 label_first.grid(row=0, column=0)
 label_middle.grid(row=1, column=0)
 label_last.grid(row=2, column=0)
@@ -36,7 +36,7 @@ entry_first.grid(row=0, column=1)
 entry_middle.grid(row=1, column=1)
 entry_last.grid(row=2, column=1)
 submit_name.grid(row=3, columnspan=2)
-# lines 38-44 positioning of the widgets within the frame_address frame
+# lines 40-46 positioning of the widgets within the frame_address frame
 label_first_line.grid(row=0, column=0)
 label_town.grid(row=1, column=0)
 label_county.grid(row=2, column=0)
@@ -45,7 +45,7 @@ entry_town.grid(row=1, column=1)
 entry_county.grid(row=2, column=1)
 submit_address.grid(row=3, columnspan=2)
 
-# lines 47 & 48 is the positioning of the frames within the windows
+# lines 49 & 50 is the positioning of the frames within the windows
 frame_name.grid(row=0, column=0)
 frame_address.grid(row=0, column=1)
 
